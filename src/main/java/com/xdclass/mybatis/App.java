84a23893384b49d43f0cf2aa94182c3182bed40d
王路路 <wllane@163.com>
2020-07-15 06:47:54 +0800

    @theme:mybatis crud

--- src/main/java/com/xdclass/mybatis/App.java
+++ src/main/java/com/xdclass/mybatis/App.java
@@ -15,6 +15,10 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -29,16 +33,67 @@ public class App {
 
             VideoMapper videoMapper = sqlSession.getMapper(VideoMapper.class);
 
-            Video video = new Video();
-            video.setTitle("小滴课堂面试专题900道");
-            video.setSummary("这是面试专题概要");
-            video.setCoverImg("xdclass.net/aaa.png");
-            video.setPrice("200");
-            video.setcId(1);
-            video.setPoint(1);
-            int count = videoMapper.add(video);
+            //Video video = videoMapper.selectById(44);
+            //System.out.println(video);
 
-            System.out.println("新增条数：" + count);
+            //List<Video> videoList = videoMapper.selectListByXML();
+            //System.out.println(videoList);
+
+            //List<Video> videoList = videoMapper.selectList();
+            //System.out.println(videoList);
+
+            //List<Video> videoList = videoMapper.selectByPointAndTitleLike(8.7, "HTML");
+            //System.out.println(videoList);
+
+            // Video video = new Video();
+            //video.setTitle("小滴课堂面试专题900道");
+            //video.setSummary("这是面试专题概要");
+            //video.setCoverImg("xdclass.net/aaa.png");
+            //video.setPrice("200");
+            //video.setcId(1);
+            //video.setPoint(1);
+            //int count = videoMapper.add(video);
+            //System.out.println("新增条数：" + count);
+
+             Video video1 = new Video();
+            video1.setTitle("小滴课堂面试专题900道111");
+            video1.setSummary("这是面试专题概要111");
+            video1.setCoverImg("xdclass.net/aaa.png");
+            video1.setPrice("200");
+            video1.setcId(1);
+            video1.setPoint(1);
+
+            Video video2 = new Video();
+            video2.setTitle("小滴课堂面试专题900道222");
+            video2.setSummary("这是面试专题概要222");
+            video2.setCoverImg("xdclass.net/aaa.png");
+            video2.setPrice("200");
+            video2.setcId(1);
+            video2.setPoint(1);
+
+            List<Video> videoList = new ArrayList<>();
+            int count = videoMapper.addBatch(videoList);
+            System.out.println("新增条数："+count);
+
+//            Video video= new Video();
+//            video.setId(49);
+//            video.setTitle("update title test");
+//            video.setCoverImg("update coverImg test");
+//            videoMapper.updateVideo(video);
+
+//            Video video= new Video();
+//            video.setId(50);
+//            video.setTitle("update title test");
+//            video.setCoverImg("update coverImg test");
+//            videoMapper.updateVideoSelective(video);
+
+            Map<String,Object> map=new HashMap<>();
+            map.put("createTime","");
+            map.put("price",9);
+            int num = videoMapper.deleteByCreateTimeAndPrice(map);
+            System.out.println("删除条数："+count);
+
+
         }
     }
 }
--- src/main/java/com/xdclass/mybatis/dao/VideoMapper.java
+++ src/main/java/com/xdclass/mybatis/dao/VideoMapper.java
@@ -2,24 +2,70 @@ package com.xdclass.mybatis.dao;
 
 import com.xdclass.mybatis.domain.Video;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
-/**
- * @author wll
- * @Description: ${todo}
- * @date 2020/7/6 23:16
- */
+import java.util.List;
+import java.util.Map;
+
 public interface VideoMapper {
 
     /**
      * @Description: 根据视频id 查找视频对象
-     * @return com.xdclass.mybatis.domain.Video
+     * @return
     */
     Video selectById(@Param("video_id") int videoId);
 
+    /**
+     * @Description: 查询全部视频列表
+     * @return
+     */
+    List<Video> selectListByXML();
+
+    /**
+     * @Description: 查询全部视频列表
+    */
+    @Select("select * from video")
+    List<Video> selectList();
+
+    /**
+     * @Description: 根据评分和标题模糊查询
+     * @param point 评分
+     * @param title 标题
+     * @return
+    */
+    List<Video> selectByPointAndTitleLike(@Param("point") double point,@Param("title")String title);
 
     /**
      * @Description: 新增一条视频记录
      * @return int
     */
     int add(Video video);
+
+    /**
+     * @Description: 批量插入
+     * @param list
+     * @return int
+    */
+    int addBatch(@Param("list")List<Video> list);
+
+    /**
+     * @Description: 更新视频
+     * @param  video
+     * @return int
+    */
+    int updateVideo(Video video);
+
+    /**
+     * @Description: 动态选择更新
+     * @param video
+     * @return int
+    */
+    int updateVideoSelective(Video video);
+
+    /**
+     * @Description: 根据时间和价格删除
+     * @param map
+     * @return int
+    */
+    int deleteByCreateTimeAndPrice(Map<String, Object> map);
 }
